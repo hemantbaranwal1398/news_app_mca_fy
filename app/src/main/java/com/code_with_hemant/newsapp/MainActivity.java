0cd91b1e7f25318a6ef4902d6ef45b8e7eb5e23e
Hemant added <hbaranwal81@gmail.com>
2021-05-03 21:07:27 +0530

    News App

---
+++ app/src/main/java/com/code_with_hemant/newsapp/MainActivity.java
@@ -0,0 +1,119 @@
+package com.code_with_hemant.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.code_with_hemant.newsapp.Model.Articles;
+import com.code_with_hemant.newsapp.Model.Headlines;
+
+import java.net.NoRouteToHostException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    SwipeRefreshLayout swipeRefreshLayout;
+    EditText etQuery;
+    Button btnSearch;
+
+    final String API_KEY = "ea79a6c09b504a3eb793e485ae78c0df";
+    Adapter adapter;
+    List<Articles> articles = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        swipeRefreshLayout = findViewById(R.id.swipeRefresh);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        etQuery = findViewById(R.id.etQuery);
+        btnSearch = findViewById(R.id.btnSearch);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        final String country = getCountry();
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                retrieveJson("", country, API_KEY);
+            }
+        });
+        retrieveJson("",country,API_KEY);
+
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!etQuery.getText().toString().equals("")){
+                    swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                        @Override
+                        public void onRefresh() {
+                            retrieveJson(etQuery.getText().toString(),country,API_KEY);
+                        }
+                    });
+                    retrieveJson(etQuery.getText().toString(),country,API_KEY);
+                }else {
+                    swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                        @Override
+                        public void onRefresh(){
+                            retrieveJson("",country,API_KEY);
+                        }
+                    });
+                    retrieveJson("",country,API_KEY);
+                }
+            }
+        });
+    }
+
+    public void retrieveJson(String query, String country, String apiKey){
+
+        swipeRefreshLayout.setRefreshing(true);
+        Call<Headlines> call;
+        if (!etQuery.getText().toString().equals("")){
+            call = ApiClient.getInstance().getApi().getSpecificData(query,apiKey);
+        }else {
+            call = ApiClient.getInstance().getApi().getHeadlines(country,apiKey);
+        }
+
+        call.enqueue(new Callback<Headlines>() {
+            @Override
+            public void onResponse(Call<Headlines> call, Response<Headlines> response) {
+                if (response.isSuccessful() && response.body().getArticles() != null){
+                    swipeRefreshLayout.setRefreshing(false);
+                    articles.clear();
+                    articles = response.body().getArticles();
+                    adapter = new Adapter(MainActivity.this,articles);
+                    recyclerView.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Headlines> call, Throwable t) {
+                swipeRefreshLayout.setRefreshing(false);
+                Toast.makeText(MainActivity.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public String getCountry(){
+        Locale locale = Locale.getDefault();
+        String country = locale.getCountry();
+        return country.toLowerCase();
+    }
+
+}
